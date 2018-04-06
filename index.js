80a7e44da4e234a55e85ef99566699f436e59215
hhaifa <haifaaziza30@gmail.com>
2018-04-06 15:25:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ require('dotenv').config();
 const express = require('express');
 
 const cors =require('cors');
-const app = express();
-const mongo = require('./providers/mongo');
 const  bodyParser =require('body-parser');
 const morgan= require('morgan');
 const boom= require('express-boom');
+
+
+
+//controllers
+const spes = require('./app/controllers/spes');
+const pays = require('./app/controllers/pays');
+const docs = require('./app/controllers/docs');
+const mailings = require('./app/controllers/mailings');
+const preinscritdocs = require('./app/controllers/preinscritdocs');
+/**
+ * Providers
+ */
+const mongo = require('./providers/mongo');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(boom());
+app.use(morgan('dev'));
 
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", '*');
@@ -17,24 +34,13 @@ app.use(function(req, res, next) {
    next();
 });
 
-//controllers
-const spes = require('./app/controllers/spes');
-const pays = require('./app/controllers/pays');
-const docs = require('./app/controllers/docs');
-const mailings = require('./app/controllers/mailings');
-const preinscritdocs = require('./app/controllers/preinscritdocs');
-
- app.use(bodyParser.json());
-app.use(boom());
-app.use(morgan('dev'));
-
 app.use('/api/v1/', pays);
 app.use('/api/v1/', spes);
 app.use('/api/v1/', docs);
 app.use('/api/v1/', mailings);
 app.use('/api/v1/', preinscritdocs);
 
-app.use(cors());
+
 mongo.check();
 //new ligne
 app.use('/', express.static('public'));
