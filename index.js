afeedd40ac523d19495556971c2e3f81632b257c
Your Namehaifa <you@example.comhaifaaziza30@gmail.com>
2018-04-12 12:11:38 +0200

    First commit

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const pays = require('./App/controllers/pays');
 const docs = require('./App/controllers/docs');
 const mailings = require('./App/controllers/mailings');
 const preinscritdocs = require('./App/controllers/preinscritdocs');
-const logins = require('./App/controllers/logins');
 /**
  * Providers
  */
@@ -40,7 +39,6 @@ app.use('/api/v1/', spes);
 app.use('/api/v1/', docs);
 app.use('/api/v1/', mailings);
 app.use('/api/v1/', preinscritdocs);
-app.use('/api/v1/', logins);
 
 mongo.check();
 //new ligne
