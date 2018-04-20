c024e8b6ab63a9a58882cfaadbee205fddc86df8
Your Namehaifa <you@example.comhaifaaziza30@gmail.com>
2018-04-20 08:24:22 +0200

    First commit

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.use(cors());
 app.use(boom());
 app.use(morgan('dev'));
 
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", '*');
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Origin-Credentials",true);
-   next();
-});
+// app.use(function(req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", '');
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.setHeader("Access-Control-Allow-Origin-Credentials",true);
+//    next();
+// });
 
 app.use('/api/v1/', pays);
 app.use('/api/v1/', spes);
