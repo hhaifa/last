c5ae4d6653e9acf76ac53f5d74557dc3dec4c979
Your Namehaifa <you@example.comhaifaaziza30@gmail.com>
2018-04-20 08:48:08 +0200

    First commit

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use('/api/v1/', logs);
 
 mongo.check();
 //new ligne
-app.use('/', express.static(__dirname +'public'));
+app.use('/', express.static('public'));
 app.listen(process.env.APP_PORT,(err)=>{
   if(err){
     console.error(err);
